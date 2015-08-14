0267355bfb2bf033137d46e01a12468adb5e1a07
Haozhe Xu <jamesxuhaozhe@gmail.com>
2015-08-13 18:20:44 -0700

    first commit

---
+++ ex2.py
@@ -0,0 +1,8 @@
+# A comment, this is so youcan read your program later.
+# Anything after the # is ignored by python.
+
+print 'I could have code like this.' # and the comment after is ignored.
+
+# You can also use a comment to "disable" or 
+
+print "This will run"
---
+++ ex5.py
@@ -0,0 +1,17 @@
+my_name = "Haozhe Xu"
+my_age = 26
+my_height = 178
+my_wight = 193
+my_eys = 'Black'
+my_teeth = 'White'
+my_hair = 'Brown'
+
+print  "Let's talk about %s." % my_name
+print "He's %d cm tall." % my_height
+print "He's %d pounds heavey." % my_wight
+print "Actually that's not too heavy."
+print "He's got %s eyes and %s hair." % (my_eys, my_hair)
+print "His teeth is actually %s depending on the coffee." % my_teeth
+
+# This line is tricky, try to get it exactly right
+print "if I add %d, %d, and %d I get %d" % (my_age, my_height, my_wight, my_age + my_height + my_wight)
